6617f37d928fe6100528b19a4670f9909913e950
Akshay-GOQii <alshay.band@ggoqii.com>
2020-01-07 16:22:22 +0530

    test

--- app/src/main/java/com/example/myapplication/ui/login/LoginActivity.kt
+++ app/src/main/java/com/example/myapplication/ui/login/LoginActivity.kt
@@ -23,7 +23,7 @@ class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+// test
         setContentView(R.layout.activity_login)
 
         val username = findViewById<EditText>(R.id.username)
